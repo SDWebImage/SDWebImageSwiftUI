1bf9417ab60ad130391ecf3bbc20ecfe0657741c
DreamPiggy <lizhuoli1126@126.com>
2020-01-27 14:55:58 +0800

    Add the Unit Test for AnimatedImage and WebImage, by using ViewInspector and Introspect

---
+++ Tests/AnimatedImageTests.swift
@@ -0,0 +1,132 @@
+import XCTest
+import SwiftUI
+import ViewInspector
+import Introspect
+@testable import SDWebImageSwiftUI
+
+extension AnimatedImage : Inspectable {}
+
+extension View {
+    func introspectAnimatedImage(customize: @escaping (SDAnimatedImageView) -> ()) -> some View {
+        return inject(IntrospectionView(
+            selector: { introspectionView in
+                guard let viewHost = Introspect.findViewHost(from: introspectionView) else {
+                    return nil
+                }
+                return Introspect.previousSibling(containing: SDAnimatedImageView.self, from: viewHost)
+            },
+            customize: customize
+        ))
+    }
+}
+
+class AnimatedImageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testAnimatedImageWithName() throws {
+        let expectation = self.expectation(description: "AnimatedImage name initializer")
+        let imageView = AnimatedImage(name: "TestImage.gif", bundle: testImageBundle())
+        let introspectView = imageView.introspectAnimatedImage { animatedImageView in
+            if let animatedImage = animatedImageView.image as? SDAnimatedImage {
+                XCTAssertEqual(animatedImage.animatedImageLoopCount, 0)
+                XCTAssertEqual(animatedImage.animatedImageFrameCount, 5)
+            } else {
+                XCTFail("SDAnimatedImageView.image invalid")
+            }
+            expectation.fulfill()
+        }
+        _ = try introspectView.inspect(AnimatedImage.self)
+        ViewHosting.host(view: introspectView)
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testAnimatedImageWithData() throws {
+        let expectation = self.expectation(description: "AnimatedImage data initializer")
+        let imageData = try XCTUnwrap(testImageData(name: "TestImageAnimated.apng"))
+        let imageView = AnimatedImage(data: imageData)
+        let introspectView = imageView.introspectAnimatedImage { animatedImageView in
+            if let animatedImage = animatedImageView.image as? SDAnimatedImage {
+                XCTAssertEqual(animatedImage.animatedImageLoopCount, 0)
+                XCTAssertEqual(animatedImage.animatedImageFrameCount, 101)
+            } else {
+                XCTFail("SDAnimatedImageView.image invalid")
+            }
+            expectation.fulfill()
+        }
+        _ = try introspectView.inspect(AnimatedImage.self)
+        ViewHosting.host(view: introspectView)
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testAnimatedImageWithURL() throws {
+        let expectation = self.expectation(description: "AnimatedImage url initializer")
+        let imageUrl = URL(string: "https://raw.githubusercontent.com/liyong03/YLGIFImage/master/YLGIFImageDemo/YLGIFImageDemo/joy.gif")
+        let imageView = AnimatedImage(url: imageUrl)
+        let introspectView = imageView.onSuccess { image, cacheType in
+            if let animatedImage = image as? SDAnimatedImage {
+                XCTAssertEqual(animatedImage.animatedImageLoopCount, 0)
+                XCTAssertEqual(animatedImage.animatedImageFrameCount, 389)
+            } else {
+                XCTFail("SDAnimatedImageView.image invalid")
+            }
+            expectation.fulfill()
+        }.onFailure { error in
+            XCTFail(error.localizedDescription)
+        }
+        _ = try introspectView.inspect(AnimatedImage.self)
+        ViewHosting.host(view: introspectView)
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testAnimatedImageBinding() throws {
+        let expectation = self.expectation(description: "AnimatedImage binding control")
+        let binding = Binding<Bool>(wrappedValue: true)
+        let imageView = AnimatedImage(name: "TestLoopCount.gif", bundle: testImageBundle(), isAnimating: binding)
+        let introspectView = imageView.introspectAnimatedImage { animatedImageView in
+            if let animatedImage = animatedImageView.image as? SDAnimatedImage {
+                XCTAssertEqual(animatedImage.animatedImageLoopCount, 1)
+                XCTAssertEqual(animatedImage.animatedImageFrameCount, 2)
+            } else {
+                XCTFail("SDAnimatedImageView.image invalid")
+            }
+            XCTAssertTrue(animatedImageView.isAnimating)
+            binding.wrappedValue = false
+            XCTAssertFalse(binding.wrappedValue)
+            XCTAssertFalse(imageView.isAnimating)
+            // TODO: current the Binding value can not been mocked, hardcode here to call `SDAnimatedImageView.stopAnimating`
+            animatedImageView.stopAnimating()
+            XCTAssertFalse(animatedImageView.isAnimating)
+            expectation.fulfill()
+        }
+        _ = try introspectView.inspect(AnimatedImage.self)
+        ViewHosting.host(view: introspectView)
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    // MARK: Helper
+    func testBundle() -> Bundle {
+        Bundle(for: type(of: self))
+    }
+    
+    func testImageBundle() -> Bundle {
+        let imagePath = (testBundle().bundlePath as NSString).appendingPathComponent("Images.bundle")
+        return Bundle(path: imagePath)!
+    }
+    
+    func testImageData(name: String) -> Data? {
+        guard let url = testImageBundle().url(forResource: name, withExtension: nil) else {
+            return nil
+        }
+        return try? Data(contentsOf: url)
+    }
+    
+}
---
+++ Tests/WebImageTests.swift
@@ -0,0 +1,63 @@
+import XCTest
+import SwiftUI
+import ViewInspector
+@testable import SDWebImageSwiftUI
+
+extension WebImage : Inspectable {}
+
+class WebImageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testWebImageWithStaticURL() throws {
+        let expectation = self.expectation(description: "WebImage static url initializer")
+        let imageUrl = URL(string: "https://nr-platform.s3.amazonaws.com/uploads/platform/published_extension/branding_icon/275/AmazonS3.png")
+        let imageView = WebImage(url: imageUrl)
+        let introspectView = imageView.onSuccess { image, cacheType in
+            let displayImage = try? imageView.inspect().group().image(0).uiImage()
+            XCTAssertNotNil(displayImage)
+            expectation.fulfill()
+        }.onFailure { error in
+            XCTFail(error.localizedDescription)
+        }
+        _ = try introspectView.inspect(WebImage.self)
+        ViewHosting.host(view: introspectView)
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testWebImageWithAnimatedURL() throws {
+        let expectation = self.expectation(description: "WebImage animated url initializer")
+        let imageUrl = URL(string: "http://apng.onevcat.com/assets/elephant.png")
+        let binding = Binding<Bool>(wrappedValue: true)
+        let imageView = WebImage(url: imageUrl, isAnimating: binding)
+        let introspectView = imageView.onSuccess { image, cacheType in
+            if let animatedImage = image as? SDAnimatedImage {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    XCTAssertTrue(imageView.isAnimating)
+                    let displayImage = try? imageView.inspect().group().image(0).uiImage()
+                    XCTAssertNotNil(displayImage)
+                    // Check display image should match the animated poster frame
+                    let posterImage = animatedImage.animatedImageFrame(at: 0)
+                    XCTAssertEqual(displayImage?.size, posterImage?.size)
+                    expectation.fulfill()
+                }
+            } else {
+                XCTFail("WebImage animated image invalid")
+            }
+        }.onFailure { error in
+            XCTFail(error.localizedDescription)
+        }
+        _ = try introspectView.inspect(WebImage.self)
+        ViewHosting.host(view: introspectView)
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+}
