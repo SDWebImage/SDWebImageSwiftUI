994058218feaba7d033fe25ad6e37c021ee70e59
Daniel Barclay <barclaysd@me.com>
2020-06-24 15:23:10 +0100

    Handle optionals more safely

--- SDWebImageSwiftUI/Classes/UIImage.swift
+++ SDWebImageSwiftUI/Classes/UIImage.swift
@@ -10,8 +10,9 @@ import UIKit
 
 extension UIImage {
     func removeWhiteBackground() -> UIImage? {
-        let image = UIImage(data: self.jpegData(compressionQuality: 1.0)!)!
-        let rawImageRef: CGImage = image.cgImage!
+        guard let rawImageRef: CGImage = self.cgImage else {
+            return nil
+        }
 
         let colorMasking: [CGFloat] = [222, 255, 222, 255, 222, 255]
         UIGraphicsBeginImageContext(image.size)
@@ -19,7 +20,7 @@ extension UIImage {
         let maskedImageRef = rawImageRef.copy(maskingColorComponents: colorMasking)
         UIGraphicsGetCurrentContext()?.translateBy(x: 0.0, y: image.size.height)
         UIGraphicsGetCurrentContext()?.scaleBy(x: 1.0, y: -1.0)
-        UIGraphicsGetCurrentContext()?.draw(maskedImageRef!, in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        UIGraphicsGetCurrentContext()?.draw(maskedImageRef!, in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
         let result = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return result
