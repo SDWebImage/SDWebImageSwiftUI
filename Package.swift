d95add347b8cd78e38a07c5113eb9035ef1e3be8
Andrew Breckenridge <asbreckenridge@me.com>
2021-03-30 00:34:34 -0700

    Package.swift: adds .dynamic library

--- Package.swift
+++ Package.swift
@@ -13,18 +13,22 @@ let package = Package(
         .library(
             name: "SDWebImageSwiftUI",
             targets: ["SDWebImageSwiftUI"]),
+        .library(
+            name: "SDWebImageSwiftUI-Dynamic",
+            type: .dynamic,
+            targets: ["SDWebImageSwiftUI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0")
+        .package(url: "https://github.com/AndrewSB/SDWebImage.git", .branch("patch-1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SDWebImageSwiftUI",
-            dependencies: ["SDWebImage"],
+            dependencies: [.product(name: "SDWebImage-Dynamic", package: "SDWebImage")],
             path: "SDWebImageSwiftUI/Classes"
         ),
     ]
