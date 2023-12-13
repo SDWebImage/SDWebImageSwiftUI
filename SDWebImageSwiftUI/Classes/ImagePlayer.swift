ce1f95b79b5750caeb013378a0436286985dc87f
DreamPiggy <lizhuoli1126@126.com>
2023-12-13 16:17:55 +0800

    Fix the Combine framework warning

--- SDWebImageSwiftUI/Classes/ImagePlayer.swift
+++ SDWebImageSwiftUI/Classes/ImagePlayer.swift
@@ -7,6 +7,7 @@
  */
 
 import SwiftUI
+import Combine
 import SDWebImage
 
 /// A Image observable object for handle aniamted image playback. This is used to avoid `@State` update may capture the View struct type and cause memory leak.
